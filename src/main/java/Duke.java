01a1b4f6f86f6056b6a2a81d4a20ea391c9e1e38
warrencxw <cxw.warren@gmail.com>
2022-01-17 17:19:09 +0800

    Add greeting and farewell messages for Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
 public class Duke {
-    public static void main(String[] args) {
+    /**
+     * Simply prints out a dividing line to standard output
+     */
+    public static void printDivider() {
+        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Prints out the greeting message to standard output
+     */
+    public static void greet() {
+        printDivider();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo + "Hello! I'm Duke\nWhat can I do for you?");
+        printDivider();
+    }
+
+    /**
+     * Prints out the farewell message to standard output
+     */
+    public static void farewell() {
+        printDivider();
+        System.out.println("Bye. Hope to see you again soon!");
+        printDivider();
+    }
+
+    public static void main(String[] args) {
+        greet();
+        farewell();
     }
 }
