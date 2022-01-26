4d690d2c6610ba97a3d07ff634c292767ea40d4d
warrencxw <cxw.warren@gmail.com>
2022-01-26 17:37:18 +0800

    Add methods to save and list user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Duke {
+    // Member attributes
+    private static int inputCount = 0;
+    private static final int MAX_COUNT = 100;
+    private static String[] userInputs = new String[MAX_COUNT];
+
     /**
      * Simply prints out a dividing line to standard output
      */
@@ -52,6 +57,37 @@ public class Duke {
         printDivider();
     }
 
+    /**
+     * Takes the provided input string and saves it into the array "userInputs"
+     * If the number of inputs exceed MAX_COUNT, the input string will not be saved.
+     */
+    public static void saveInput(String input) {
+        if (inputCount >= MAX_COUNT) {
+            System.out.println("We have reached the limit of " + MAX_COUNT +
+                    " items that can be added to the list.\n" +
+                    "The last entry, \"" + input + "\", will not be added to the list.");
+        }
+        else {
+            userInputs[inputCount] = input;
+            inputCount++;
+            System.out.println("\"" + input + "\" has been added into the list.");
+        }
+    }
+
+    /**
+     *  Prints out the full list of inputs given by the user
+     */
+    public static void printInputList() {
+        for (int i = 0; i < inputCount; i++) {
+            System.out.println((i + 1) + ". " + userInputs[i]);
+        }
+    }
+
+    /**
+     * Reads in input from standard input as long as the user does not
+     * type in "bye", and processes the input accordingly by calling other
+     * helper methods.
+     */
     public static void processInput() {
         Scanner in = new Scanner(System.in);
         System.out.print(" > ");
@@ -59,7 +95,14 @@ public class Duke {
 
         while (!input.equals("bye")) {
             printDivider();
-            System.out.println(input);
+            switch (input) {
+            case "list":
+                printInputList();
+                break;
+            default:
+                saveInput(input);
+                break;
+            }
             printDivider();
             System.out.print(" > ");
             input = in.nextLine();
