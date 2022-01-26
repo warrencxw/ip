2e8fd2490665140ee4e0853cbd26752394569f0f
warrencxw <cxw.warren@gmail.com>
2022-01-26 17:12:56 +0800

    Update method names and add input processing method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     /**
      * Simply prints out a dividing line to standard output
@@ -9,28 +11,64 @@ public class Duke {
     /**
      * Prints out the greeting message to standard output
      */
-    public static void greet() {
+    public static void printGreet() {
         printDivider();
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo + "Hello! I'm Duke\nWhat can I do for you?");
+        String logo =
+                "                      /|      __\n" +
+                "*             +      / |   ,-~ /             +\n" +
+                "     .              Y :|  //  /                .         *\n" +
+                "         .          | jj /( .^     *\n" +
+                "               *    >-\"~\"-v\"              .        *        .\n" +
+                "*                  /       Y\n" +
+                "   .     .        jo  o    |     .            +\n" +
+                "                 ( ~T~     j                     +     .\n" +
+                "      +           >._-' _./         +\n" +
+                "               /| ;-\"~ _  l\n" +
+                "  .           / l/ ,-\"~    \\     +\n" +
+                "              \\//\\/      .- \\\n" +
+                "       +       Y        /    Y\n" +
+                "               l       I     !\n" +
+                "               ]\\      _\\    /\"\\\n" +
+                "              (\" ~----( ~   Y.  )\n" +
+                "          ~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        String warrenAscii =
+                ",--.   ,--.                                     \n" +
+                "|  |   |  | ,--,--.,--.--.,--.--. ,---. ,--,--,  \n" +
+                "|  |.'.|  |' ,-.  ||  .--'|  .--'| .-. :|      \\\n" +
+                "|   ,'.   |\\ '-'  ||  |   |  |   \\   --.|  ||  |\n" +
+                "'--'   '--' `--`--'`--'   `--'    `----'`--''--'\n";
+        System.out.println(logo);
+        printDivider();
+        System.out.println("Hello,\n" + warrenAscii + "What can I do for you?");
         printDivider();
     }
 
     /**
      * Prints out the farewell message to standard output
      */
-    public static void farewell() {
+    public static void printFarewell() {
         printDivider();
         System.out.println("Bye. Hope to see you again soon!");
         printDivider();
     }
 
+    public static void processInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.print(" > ");
+        String input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            printDivider();
+            System.out.println(input);
+            printDivider();
+            System.out.print(" > ");
+            input = in.nextLine();
+        }
+        printFarewell();
+    }
+
     public static void main(String[] args) {
-        greet();
-        farewell();
+        printGreet();
+        processInput();
     }
 }
