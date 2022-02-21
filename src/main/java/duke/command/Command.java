c40daf8a147900c435b5c1e75aa5074a5295513b
warrencxw <cxw.warren@gmail.com>
2022-02-21 17:06:02 +0800

    Add Command abstract class as basis for other commands

---
+++ src/main/java/duke/command/Command.java
@@ -0,0 +1,24 @@
+package duke.command;
+
+import duke.Display;
+import duke.Storage;
+import duke.task.TaskList;
+
+public abstract class Command {
+    public enum CommandType {
+        EMPTY, HELP, LIST, MARK, TODO, DEADLINE, EVENT, DELETE, CLEAR, EXIT, UNRECOGNISED
+    }
+    
+    // Member attributes
+    protected String commandArgs;
+    
+    public abstract void run(Display ui, TaskList tasks, Storage storage);
+    
+    public Command(String commandArgs) {
+        this.commandArgs = commandArgs;
+    }
+    
+    public Command() {
+        this.commandArgs = "";
+    }
+}
