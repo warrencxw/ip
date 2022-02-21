7e42fd7309228a0577ef89477346364d0b1634e1
warrencxw <cxw.warren@gmail.com>
2022-02-21 17:31:54 +0800

    Rewrite processInputLoop method in Duke to entrust parsing to Parser
    Shift TaskList clearing confirmation logic to ClearCommand class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -26,81 +26,18 @@ public class Duke {
      * what operations to carry out by calling other helper methods.
      * Repeats process until exit/quit/bye commands are called.
      */
-    public static void processInputLoop() {
-        Scanner in = new Scanner(System.in);
-        System.out.print(INPUT_PREPEND);
-        String input = in.nextLine().trim();
-        // inputs: Array of Strings { command, furtherArguments }
-        String[] inputs = input.split(REGEX_PATTERN_WHITESPACES, 2);
-
-        boolean isCommandOnly;
-        while (inputs.length > 0) {
-            Display.printDivider();
-            switch (inputs[0].toLowerCase()) {
-            // EMPTY INPUT
-            case "":
-                Display.printError(Display.ErrorType.EMPTY_INPUT);
-                break;
-            // DISPLAY HELP MENU, FALLTHROUGH : IDENTICAL COMMAND
-            case "?":
-            case "help":
-                Display.printHelpMessage();
-                break;
-            // LIST OUT TASK LIST
-            case "list":
-                TaskList.printTaskList();
-                break;
-            // MARK TASK AS DONE
-            case "mark":
-                TaskList.processInputAndMarkTask(true, inputs);
-                SaveManager.saveChanges();
-                break;
-            // MARK TASK AS NOT DONE
-            case "unmark":
-                TaskList.processInputAndMarkTask(false, inputs);
-                SaveManager.saveChanges();
-                break;
-            // CREATE NEW TODO
-            case "todo":
-                TaskList.saveInputAsTask(inputs, TaskList.TaskType.TODO);
-                SaveManager.saveChanges();
-                break;
-            // CREATE NEW DEADLINE
-            case "deadline":
-                TaskList.saveInputAsTask(inputs, TaskList.TaskType.DEADLINE);
-                SaveManager.saveChanges();
-                break;
-            // CREATE NEW EVENT
-            case "event":
-                TaskList.saveInputAsTask(inputs, TaskList.TaskType.EVENT);
-                SaveManager.saveChanges();
-                break;
-            // DELETE TASK, FALLTHROUGH : IDENTICAL COMMANDS
-            case "delete":
-            case "remove":
-                TaskList.processInputAndDeleteTask(inputs);
-                SaveManager.saveChanges();
-                break;
-            case "clear":
-                TaskList.removeAllTasks(in);
-                break;
-            // CONCLUDE SESSION, FALLTHROUGH : IDENTICAL COMMANDS
-            case "exit":
-            case "quit":
-            case "bye":
-                Display.printFarewellMessage();
-                return;
-            // UNRECOGNISED INPUT
-            default:
-                Display.printError(Display.ErrorType.COMMAND_NOT_RECOGNISED);
-                break;
-            }
-            Display.printDivider();
-            System.out.print(INPUT_PREPEND);
-
-            input = in.nextLine().trim();
-            inputs = input.split(REGEX_PATTERN_WHITESPACES, 2);
-        }
+    public void processInputLoop() {
+        String input;
+        Command command;
+        
+        do {
+            ui.printMessage(INPUT_PREPEND);
+            input = ui.getNextLine();
+            ui.printDivider();
+            command = Parser.getCommand(input);
+            command.run(ui, tasks, storage);
+            ui.printDivider();
+        } while (!(command instanceof ExitCommand));
     }
 
     public Duke(String saveFileName) {
